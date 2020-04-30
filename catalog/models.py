92dcde305e9274e74a783fc4849f799c0acb04ca
Мария Калинкина <mashyfycya@gmail.com>
2020-04-30 11:58:01 +0300

    Models declaration

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class BookLanguage(models.Model):
+    pass
+
+
+class BookGenre(models.Model):
+    pass
+
+
+class Author(models.Model):
+    pass
+
+
+class Book(models.Model):
+    pass
+
+
+class BookInstance(models.Model):
+    pass
