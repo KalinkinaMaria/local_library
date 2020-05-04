5e8f4658b7f84d05fda8c9f60f1bd7948904869b
Мария Калинкина <mashyfycya@gmail.com>
2020-05-04 18:11:38 +0300

    Create catalog home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    num_books = models.Book.objects.all().count()
+    num_instances = models.BookInstance.objects.all().count()
+
+    num_instances_available = models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors = models.Author.objects.count()
+    
+    # Отрисовка HTML-шаблона index.html с данными внутри 
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors
+        },
+    )
